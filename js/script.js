d67542eb1f9ae8f83b1226faff41bcd18b8c1aa6
David Henry <davidalexanderhenry@Outlook.com>
2022-04-25 14:34:54 -0400

    Init

---
+++ js/script.js
@@ -0,0 +1,43 @@
+// menu
+$(document).ready(function() {
+
+    $nav = $('.nav');
+    $toggle = $('.menu');
+
+    $toggle.click(function() {
+        $nav.toggleClass('collapse');
+    })
+
+
+});
+
+// for the carousel
+$('.owl-carousel').owlCarousel({
+        loop: true,
+        margin: 10,
+        smartSpeed: 1300,
+        autoplay: true,
+        autoplayTimeout: 5000,
+        autoplayHoverPause: true,
+        dotsEach: true,
+        lazyLoad: true,
+
+
+
+        responsive: {
+            0: {
+                items: 1
+
+            },
+            600: {
+                items: 2
+            },
+            1000: {
+                items: 4
+            }
+        }
+    }
+
+
+
+)
